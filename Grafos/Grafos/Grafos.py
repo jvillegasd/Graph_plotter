28ff97af7b583e6b9a552dfadf1e3845e88978de
Johnny Villegas <linkrsfan@gmail.com>
2018-04-22 12:47:07 -0500

    Add files via upload

--- Grafos/Grafos/Grafos.py
+++ Grafos/Grafos/Grafos.py
@@ -2,6 +2,13 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 
+def printAdj(Adj, tipo):
+    if tipo==1:
+        print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in Adj]))
+    else:
+        for i in range(len(Adj[0])):
+            print('(',Adj[0][i],',',Adj[1][i],')->',Adj[2][i])
+
 def drawGraph(G, longestPath):
     for e in G.edges():
         G[e[0]][e[1]]['color']='black'
@@ -29,6 +36,7 @@ def adjInput(m):
     for row in rows:
         strAdj.append(row.split(" "))
     newAdj=strToInt(strAdj)
+    printAdj(newAdj, 1)
     Adj=np.matrix(newAdj)
     return Adj
 
@@ -38,6 +46,7 @@ def sparseInput(m, n):
     for row in rows:
         strAdj.append(row.split(" "))
     newAdj=strToInt(strAdj)
+    printAdj(newAdj, 2)
     Adj=[[0]*n for i in range(n)]
     for i in range(len(strAdj[0])):
         Adj[newAdj[0][i]-1][newAdj[1][i]-1]=newAdj[2][i]
@@ -46,13 +55,15 @@ def sparseInput(m, n):
 tipo=int(input('1) Matriz de Adjacencia de pesos\n2) Lista de Adjacencia con pesos\nDigite opcion: '))
 if tipo==1:
     m=input('Digite Matriz de Adjacencia de pesos: ')
-    Adj=adjInput(m)
+    print('\nMatriz de Adjacencia de pesos:')
+    Adj=adjInput(m) 
 else:
     m=input('Digite Lista de Adjacencia de pesos: ')
     n=int(input('Digite numero de nodos: '))
-    Adj=sparseInput(m, n)
+    print('\nAristas con sus pesos digitados:')
+    Adj=sparseInput(m, n)  
 G=nx.from_numpy_matrix(Adj, create_using=nx.DiGraph())
-S=int(input('Digite el nodo fuente: '))
+S=int(input('\nDigite el nodo fuente: '))
 pred,lenght=nx.dijkstra_predecessor_and_distance(G, S)
 path=nx.single_source_dijkstra_path(G, S)
 print('\nDistancia a todos los nodos desde el nodo fuente', S,'usando Dijkstra:')
@@ -68,6 +79,10 @@ for i in range(0, Adj.shape[0]):
     if(i!=S): print(str(pred[i]).strip("[]")," ", end='')
     else: print("-1 ", end=' ')
     if(i==Adj.shape[0]-1): print()
+print('\nTodos los caminos cortos desde el nodo fuente', S,'usando Dijkstra:')
+for key in path:
+    print('Camino hacia',key)
+    print(path[key])
 drawGraph(G, list(path[v]))
 #Ejemplos de como se debe digitar la matriz de adj y la lista de adj:
 #0 10 5 0 0;0 0 2 1 0;0 3 0 9 2;0 0 0 0 4;7 0 0 6 0 matriz
